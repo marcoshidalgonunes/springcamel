e40c14d5bb320256211b8213e026b2ce526c5906
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2023-06-15 18:51:28 -0300

    Minor refactoring

--- src/main/java/com/springcamel/routes/RestApiRoute.java
+++ src/main/java/com/springcamel/routes/RestApiRoute.java
@@ -4,7 +4,6 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.rest.RestBindingMode;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.springcamel.components.InterceptProcessor;
@@ -12,14 +11,11 @@ import com.springcamel.models.Greeting;
 
 @Component
 class RestApiRoute extends RouteBuilder {
-    @Value("${server.port}")
-    String serverPort;
 
     @Override
     public void configure() throws Exception  {
         try (CamelContext context = new DefaultCamelContext()) {
             restConfiguration()
-                .port(serverPort)
                 .enableCORS(true)
                 .bindingMode(RestBindingMode.json);  
 
