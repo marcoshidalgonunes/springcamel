ebed432b62976804913ebe9158002fd88fbb2b14
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2023-08-09 17:25:51 -0300

    Identifies endpoint processed

--- src/main/java/com/springcamel/processors/InterceptProcessor.java
+++ src/main/java/com/springcamel/processors/InterceptProcessor.java
@@ -1,18 +1,25 @@
 package com.springcamel.processors;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
 public class InterceptProcessor implements Processor {
 	
-	private int count;
+	private Map<String,Integer> map = new HashMap<String, Integer>();
 
     @Override
     public void process(Exchange exchange) throws Exception {
         String uri = exchange.getProperty(Exchange.TO_ENDPOINT, String.class);
         if (!(uri == null || uri.isBlank())) {
-            count++;
-            System.out.println("'" + uri + "'' route called " + count + " times ");        
+            if (!map.containsKey(uri)) {
+                map.put(uri, 0);
+            }
+            int count = map.get(uri) + 1;
+            map.replace(uri, count);
+            System.out.println("'" + uri + "' route called " + count + " times ");        
         }
     }
 }
