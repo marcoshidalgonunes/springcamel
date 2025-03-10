042f8063300ac5f3b4e02fe3819f47aeb719ee3d
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2025-03-10 16:09:58 -0300

    Updated dependencies

--- src/main/java/com/springcamel/routes/RestApiRoute.java
+++ src/main/java/com/springcamel/routes/RestApiRoute.java
@@ -1,8 +1,6 @@
 package com.springcamel.routes;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,15 +8,13 @@ class RestApiRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception  {
-        try (CamelContext context = new DefaultCamelContext()) {
-            restConfiguration()
-               // and output using pretty print
-                .dataFormatProperty("prettyPrint", "true")
-                // add swagger api-doc out of the box
-                .apiContextPath("/docs")
-                    .apiProperty("api.title", "User API").apiProperty("api.version", "1.2.3")
-                    // and enable CORS
-                    .apiProperty("cors", "true");                    
-        } 
+        restConfiguration()
+        // and output using pretty print
+         .dataFormatProperty("prettyPrint", "true")
+         .enableCORS(true)
+         // add swagger api-doc out of the box
+         .apiContextPath("/api-doc")
+             .apiProperty("api.title", "User API")
+             .apiProperty("api.version", "1.2.3");
     }
 }
